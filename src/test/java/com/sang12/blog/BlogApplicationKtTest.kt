ba89ad72ccbe9c8d8cee006afcea1ea84b5298d8
ChoiSangIl <wo566@naver.com>
2022-09-14 09:11:52 +0900

    feat: add kotest testing style

--- src/test/java/com/sang12/blog/BlogApplicationKtTest.kt
+++ src/test/java/com/sang12/blog/BlogApplicationKtTest.kt
@@ -1,19 +1,43 @@
 package com.sang12.blog
 
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldNotBe
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
-import org.springframework.test.context.ContextConfiguration
 
+/**
+ * kotest testing style
+ * https://kotest.io/docs/framework/testing-styles.html
+ */
 @SpringBootTest
-@ContextConfiguration
 class BlogApplicationKtTest(
     private val context: ApplicationContext
 ) : StringSpec({
-
-    "should return the greeting provided by greeting service" {
+    "Spring Boot 서비스가 실행 됐을경우, ApllicationContext 객체를 반환해야 한다" {
         context shouldNotBe null
     }
+})
 
+@SpringBootTest
+class BlogApplicationKtShouldSpec(
+    private val context: ApplicationContext
+) : ShouldSpec({
+    should("Spring Boot 서비스가 실행 됐을경우, ApllicationContext 객체를 반환해야 한다"){
+        context shouldNotBe null
+    }
+})
+
+@SpringBootTest
+class BlogApplicationKtBehaviorSpec(
+    private val context: ApplicationContext
+) : BehaviorSpec({
+    given("context 객체가 주어 졌을 때") {
+        `when`("스프링부트 서비스가 올라갈 때") {
+            then("context 객체를 반환해야한다") {
+                context shouldNotBe null
+            }
+        }
+    }
 })
